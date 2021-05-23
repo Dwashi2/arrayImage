ae424e95252eb22e61a8ce4bdd4fb9378b9dd47d
Dwashi2 <67284604+Dwashi2@users.noreply.github.com>
2021-05-23 20:40:01 -0300

    Add files via upload

--- inputArrayImage/ViewController.swift
+++ inputArrayImage/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var imageArray: [UIImage?] = [UIImage(named: "broly"),UIImage(named: "goku"),UIImage(named: "vegeta"),UIImage(named: "bulma"),UIImage(named: "garlicjr"), UIImage(named: "cell"),UIImage(named: "babidi"),UIImage(named: "frieza"),UIImage(named: "dabura")]
+    var info:[String] = ["Broly", "Goku", "Vegeta", "Bulma", "Garlic Jr", "Cell", "Babidi" , "Frieza", "Dabura"]
+    
+    @IBOutlet weak var showImage: UIImageView!
+    @IBOutlet weak var inputText: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    var valueForImage = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func show(_ sender: Any) {
+        self.resultImage()
+        }
+        
+        
+        
+    func resultImage(){
+        let newText = inputText.text
+        for value in info{
+            if newText == value{
+                showImage.image = imageArray[valueForImage]
+            }
+            valueForImage = valueForImage + 1
+            }
+        valueForImage = 0
+        inputText.text = ""
+    }
+    
+    
 
 }
 
